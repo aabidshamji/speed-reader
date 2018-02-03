4395bf14551ec088aa0ce9e5c8b399cdfed43185
Aabid Shamji <aabidshamji@gmail.com>
2018-02-02 23:02:02 -0600

    centered the text and colored the letters

--- src/SpeedReader.java
+++ src/SpeedReader.java
@@ -1,58 +1,153 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.io.File;
 import java.io.IOException;
 
 public class SpeedReader {
 	public static void SpeedReader1 (String filename, int width, int height, int fsize, int wpm) throws IOException, InterruptedException {
-		
-	
+
 		DrawingPanel panel = new DrawingPanel(width, height);
-	    Graphics g = panel.getGraphics();
-	    Font f = new Font("Courier", Font.BOLD, fsize);
-	    g.setFont(f);
-	    g.clearRect(0, 0, 400, 300);
-	    g.setColor(Color.WHITE);
-	    
-	    WordGenerator test = new WordGenerator(filename);
-	        while(test.hasNext()) {	        	
-	            g.drawString(test.next(), 100, 100);	      
-	            Thread.sleep((60/wpm)*1000);          
-	            g.clearRect(0, 0, width, height);
-   
-	    }
-	        
-	        g.drawString("Number of words processed: " + test.getWordCount(), 100, 100);
-	        g.drawString("Number of sentences processed: " + test.getSentenceCount(), 100, 200);
+		Graphics g = panel.getGraphics();
+		Font f = new Font("Courier", Font.BOLD, fsize);
+		g.setFont(f);
+		g.clearRect(0, 0, 400, 300);
+		g.setColor(Color.WHITE);
+
+		WordGenerator test = new WordGenerator(filename);
+		FontMetrics fontSpace = g.getFontMetrics(f);
+		
+		while(test.hasNext()) {	  
+			
+			String curr = test.next();
+			int currHeight = fontSpace.getHeight();
+			int currLength = fontSpace.stringWidth(curr);
+			int cWidth = (width/2) - (currLength/2);
+			int cLen = (height/2) - (currHeight/2);
+			int sLen = curr.length();
+
+			char[] charArr = curr.toCharArray();
+
+			if (sLen <= 2) {
+				for (int i = 0; i < sLen; i++) {
+					if (i == 0) {
+						g.setColor(Color.RED);
+						g.drawString(Character.toString(charArr[i]), cWidth, cLen);
+						g.setColor(Color.WHITE);
+						cWidth += fontSpace.charWidth(charArr[i]);
+					} 
+
+					else {
+						g.drawString(Character.toString(charArr[i]), cWidth, cLen);
+						cWidth += fontSpace.charWidth(charArr[i]);
+					} 
+				}
+			} 
+
+			else if (sLen <= 5) {
+				for (int i = 0; i < sLen; i++) {
+					if (i == 1) {
+						g.setColor(Color.RED);
+						g.drawString(Character.toString(charArr[i]), cWidth, cLen);
+						g.setColor(Color.WHITE);
+						cWidth += fontSpace.charWidth(charArr[i]);
+					} 
+
+					else {
+						g.drawString(Character.toString(charArr[i]), cWidth, cLen);
+						cWidth += fontSpace.charWidth(charArr[i]);
+					} 
+				}
+			} 
+
+			else if (sLen <= 9) {
+				for (int i = 0; i < sLen; i++) {
+					if (i == 2) {
+						g.setColor(Color.RED);
+						g.drawString(Character.toString(charArr[i]), cWidth, cLen);
+						g.setColor(Color.WHITE);
+						cWidth += fontSpace.charWidth(charArr[i]);
+					} 
+
+					else {
+						g.drawString(Character.toString(charArr[i]), cWidth, cLen);
+						cWidth += fontSpace.charWidth(charArr[i]);
+					} 
+				}
+			} 
+
+			else if (sLen <= 13) {
+				for (int i = 0; i < sLen; i++) {
+					if (i == 3) {
+						g.setColor(Color.RED);
+						g.drawString(Character.toString(charArr[i]), cWidth, cLen);
+						g.setColor(Color.WHITE);
+						cWidth += fontSpace.charWidth(charArr[i]);
+					} 
+
+					else {
+						g.drawString(Character.toString(charArr[i]), cWidth, cLen);
+						cWidth += fontSpace.charWidth(charArr[i]);
+					} 
+				} 
+			} 
+
+			else {
+				for (int i = 0; i < sLen; i++) {
+					if (i == 4) {
+						g.setColor(Color.RED);
+						g.drawString(Character.toString(charArr[i]), cWidth, cLen);
+						g.setColor(Color.WHITE);
+						cWidth += fontSpace.charWidth(charArr[i]);
+					} 
+
+					else {
+						g.drawString(Character.toString(charArr[i]), cWidth, cLen);
+						cWidth += fontSpace.charWidth(charArr[i]);
+					} 
+				}
+			}
+
+			//g.drawString(curr, cWidth, cLen);	      
+			Thread.sleep( (long) (((double) 60 / (double) wpm) * 1000.00));          
+			g.clearRect(0, 0, width, height);
+		}
+		
+		String totalWords = "Words Processed: " + test.getWordCount();
+		String totalSent = "Sentences Processed: " + test.getSentenceCount();
+		
+		g.drawString(totalWords, (width/2)-(fontSpace.stringWidth(totalWords)/2), (height/2)-fontSpace.getHeight());
+		g.drawString(totalSent, (width/2)-(fontSpace.stringWidth(totalSent)/2), (height/2)+fontSpace.getHeight());
 	}
+
+	
 	
 	public static boolean check (int x) {
-		if (x <= 0) {
-			return false;
-		}
-		else {
-			return true;
-		}
+		if (x <= 0) { return false; }
+		else { return true; }
 	}
+	
+	
+	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		System.out.println(args.length);
+		
 		if (args.length != 5) {	
 			System.out.println("Usage: SpeedReader <filename> <width> <height> <font size> <wpm>");
-			
 			System.exit(0);
 		}
-		
+
 		int width = Integer.parseInt(args[1]);
 		int heigth = Integer.parseInt(args[2]);
 		int fontSize = Integer.parseInt(args[3]);
 		int wpm = Integer.parseInt(args[4]);
-		
+
 		File temp = new File (args[0]);
 		if (!temp.exists()) {
 			System.out.println("The file does not exist");
 		}
-		
+
 		if (!(check(width) && 
 				check(heigth) && 
 				check(fontSize) && 
